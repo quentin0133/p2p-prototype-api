e90c972ce94a791f5c1f66932e80a9a71ff0d91d
Quentin Yahia <quentin.yahia.pro@gmail.com>
2025-08-11 14:02:01 +0200

    add CORS config :wrench:

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import LobbyRoute from './routes/lobby-route';
 import { errorHandler } from './middlewares/errors/error-handle';
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+}))
 
 app.use(express.json());
 
 app.use(errorHandler);
 
 app.use('/lobbies', LobbyRoute);
-
-app.set('trust proxy', true);
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -7,8 +7,6 @@ interface Config {
     nodeEnv: string;
 }
 
-console.log(process.env.PORT);
-
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || 'development',
