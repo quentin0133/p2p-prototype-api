c8b4d7c8a110494a05529a83623a8fcf3d0859ae
Quentin Yahia <quentin.yahia.pro@gmail.com>
2025-08-11 13:42:32 +0200

    logging on the port :loud_sound:

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,8 @@ interface Config {
     nodeEnv: string;
 }
 
+console.log(process.env.PORT);
+
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || 'development',
